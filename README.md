ee1aa8cb7044097c0445a7b4c825a23604460424
vianey <vianymarcelo@gmail.com>
2025-09-01 15:34:40 -0600

    start project

---
+++ README.md
@@ -0,0 +1,45 @@
+# 🚀 Quick start
+
+## Example Site
+[![](screenshots.png)](https://okvianey.github.io/himnappweb/)
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Pages](#pages)
+  - [Resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### Links
+
+👉🏻 Code: [Code URL](https://github.com/okvianey/himnapp)
+
+👉🏻 Live Site URL: [View Live Preview](https://okvianey.github.io/himnapp/)
+
+## My process
+
+### Pages
+
+* **Homepage**
+* **About**
+* **Hymn page**
+
+<!-- resources -->
+### Resources
+
+Some third-party plugins that we used to build this template. Please check their license.
+
+* **Google Fonts**: <http://fonts.google.com/>
+
+## Author
+
+☕️ Made with coffee & code by [okvianey](https://okvianey.github.io)
+
+- Linkedin - [@vianydev](https://www.linkedin.com/in/vianydev/)
+- Github - [@okvianey](https://github.com/okvianey)
+- Instagram - [@okvianey](https://instagram.com/okvianey)
+
---
+++ assets/js/main.js
@@ -0,0 +1,164 @@
+(function() {
+  "use strict";
+
+  /**
+   * Apply .scrolled class to the body as the page is scrolled down
+   */
+  function toggleScrolled() {
+    const selectBody = document.querySelector('body');
+    const selectHeader = document.querySelector('#header');
+    if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
+    window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
+  }
+
+  document.addEventListener('scroll', toggleScrolled);
+  window.addEventListener('load', toggleScrolled);
+
+  /**
+   * Mobile nav toggle
+   */
+  const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
+
+  function mobileNavToogle() {
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    mobileNavToggleBtn.classList.toggle('bi-list');
+    mobileNavToggleBtn.classList.toggle('bi-x');
+  }
+  if (mobileNavToggleBtn) {
+    mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
+  }
+
+  /**
+   * Hide mobile nav on same-page/hash links
+   */
+  document.querySelectorAll('#navmenu a').forEach(navmenu => {
+    navmenu.addEventListener('click', () => {
+      if (document.querySelector('.mobile-nav-active')) {
+        mobileNavToogle();
+      }
+    });
+
+  });
+
+  /**
+   * Toggle mobile nav dropdowns
+   */
+  document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
+    navmenu.addEventListener('click', function(e) {
+      e.preventDefault();
+      this.parentNode.classList.toggle('active');
+      this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
+      e.stopImmediatePropagation();
+    });
+  });
+
+  /**
+   * Preloader
+   */
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove();
+    });
+  }
+
+  /**
+   * Scroll top button
+   */
+  let scrollTop = document.querySelector('.scroll-top');
+
+  function toggleScrollTop() {
+    if (scrollTop) {
+      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+    }
+  }
+  scrollTop.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+
+  window.addEventListener('load', toggleScrollTop);
+  document.addEventListener('scroll', toggleScrollTop);
+
+  /**
+   * Init swiper sliders
+   */
+  function initSwiper() {
+    document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
+      let config = JSON.parse(
+        swiperElement.querySelector(".swiper-config").innerHTML.trim()
+      );
+
+      if (swiperElement.classList.contains("swiper-tab")) {
+        initSwiperWithCustomPagination(swiperElement, config);
+      } else {
+        new Swiper(swiperElement, config);
+      }
+    });
+  }
+
+  window.addEventListener("load", initSwiper);
+
+  /**
+   * Initiate glightbox
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+
+    /**
+   * Manejo del formulario de contacto
+   */
+  function initContactForm() {
+    const form = document.getElementById("my-form");
+    
+    if (!form) return; // Si no existe el formulario, salir
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      const status = document.getElementById("my-form-status");
+      const data = new FormData(event.target);
+      
+      try {
+        const response = await fetch(event.target.action, {
+          method: form.method,
+          body: data,
+          headers: {
+            'Accept': 'application/json'
+          }
+        });
+        
+        if (response.ok) {
+          status.innerHTML = "¡Gracias por ponerte en contacto!";
+          status.classList.add('form-status', 'success');
+          form.reset();
+        } else {
+          const data = await response.json();
+          if (Object.hasOwn(data, 'errors')) {
+            status.innerHTML = data["errors"].map(error => error["message"]).join(", ");
+          } else {
+            status.innerHTML = "¡Ups! Hubo un problema con tu formulario.";
+          }
+          status.classList.add('form-status', 'error');
+        }
+      } catch (error) {
+        status.innerHTML = "¡Ups! Hubo un problema al enviar tu formulario.";
+        status.classList.add('form-status', 'error');
+      }
+    }
+    
+    form.addEventListener("submit", handleSubmit);
+  }
+
+  // Inicializar el formulario cuando el DOM esté listo
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', initContactForm);
+  } else {
+    initContactForm();
+  }
+})();
+
